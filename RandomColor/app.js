125e70e9850d20436e4932dbe3e5b0ffb14dd5f0
anik-23 <anik23jan@gmail.com>
2022-04-20 23:15:56 +0530

    create js

---
+++ RandomColor/app.js
@@ -0,0 +1,15 @@
+const btn=document.querySelector('button');
+const h1=document.querySelector('h1');
+
+btn.addEventListener('click',()=>{
+    const newColor=makeRandColor();
+    document.body.style.backgroundColor=newColor;
+    h1.innerText=newColor;
+})
+
+const makeRandColor=()=>{
+    const r=Math.floor(Math.random() *256);
+    const g=Math.floor(Math.random() *256);
+    const b=Math.floor(Math.random() *256);
+    return `rgb(${r} , ${g} , ${b})`;
+}
